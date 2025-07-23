7c46655baace0fe73dad8f9c989a54b78e9f13c6
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-07-22 21:32:54 -0300

    add unbound method reference example

--- src/main/java/lambda/MethodReferenceTypes.java
+++ src/main/java/lambda/MethodReferenceTypes.java
@@ -1,11 +1,14 @@
 package lambda;
 
+import java.util.function.BiFunction;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 public class MethodReferenceTypes {
     public static void main(String[] args) {
         showBoundMethodReferences();
+        showUnboundMethodReferences();
     }
 
     private static void showBoundMethodReferences() {
@@ -15,12 +18,27 @@ public class MethodReferenceTypes {
         Supplier<String> lowerMethodReference = name::toLowerCase;
 
         System.out.println("Lambda: " + lowerLambda.get());
-        System.out.println("Method Reference" + lowerMethodReference.get());
+        System.out.println("Method Reference: " + lowerMethodReference.get());
 
         Predicate<String> titleLambda = (title) -> name.startsWith(title);
         Predicate<String> titleMethodReference = name::startsWith;
 
         System.out.println("Lambda: " + titleLambda.test("Mr."));
-        System.out.println("Method Reference" + titleMethodReference.test("Ms."));
+        System.out.println("Method Reference: " + titleMethodReference.test("Ms."));
     }
+
+    private static void showUnboundMethodReferences() {
+        Function<String, String> upperLambda = s -> s.toUpperCase();
+        Function<String, String> upperMethodReference = String::toUpperCase;
+
+        System.out.println("Lambda: " + upperLambda.apply("calebe"));
+        System.out.println("Method Reference: " + upperMethodReference.apply("calebe"));
+
+        BiFunction<String, String, String> concatLambda = (s1, s2) -> s1.concat(s2);
+        BiFunction<String, String, String> concatMethodReference = String::concat;
+
+        System.out.println("Lambda: " + concatLambda.apply("Calebe ", "Oliveira"));
+        System.out.println("Method Reference: " + concatMethodReference.apply("Sean ", "Kennedy"));
+    }
+
 }
