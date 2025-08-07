eb9b4f22f253745225c8954b8340d149229c8d9f
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-08-07 20:20:08 -0300

    add polymorphic issue with generics

---
+++ src/main/java/generics/PolymorphicIssueWIthGenerics.java
@@ -0,0 +1,25 @@
+package generics;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+class PolymorphicIssueWIthGenerics {
+    public static void main(String[] args) {
+        // The issue
+        List<Double> doubles = new ArrayList<>();
+        doubles.add((12.3);
+//        List<Object> objects = doubles; // COMPILER ERROR
+
+        // A different variation
+        List<String> names = new ArrayList<>();
+        names.add("Sean");
+//        showList(names);
+    }
+
+    private static void showList(List<Object> list) {
+        for(Object object: list) {
+            System.out.println(object);
+        }
+    }
+}
