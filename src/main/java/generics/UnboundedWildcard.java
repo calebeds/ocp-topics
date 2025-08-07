0b78f99725c0951442bdb9761d99c0b21777cd2f
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-08-07 20:13:39 -0300

    add unbounded wild card

---
+++ src/main/java/generics/UnboundedWildcard.java
@@ -0,0 +1,25 @@
+package generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class UnboundedWildcard {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("Sean");
+        showList(names);
+
+        List<Dog> dogs = new ArrayList<>();
+        dogs.add(new Dog());
+        showList(dogs);
+    }
+
+//    private static void showList(List<Object> list) {
+    private static void showList(List<?> list) {
+        for(Object object: list) {
+            System.out.println(object);
+        }
+
+//        list.add("test"); // <?> imples read-only
+    }
+}
