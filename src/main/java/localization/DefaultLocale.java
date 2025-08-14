7aa85c22b97c01a6fff8689b4a922583785bf79a
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-08-14 17:56:36 -0300

    add default locale example

---
+++ src/main/java/localization/DefaultLocale.java
@@ -0,0 +1,10 @@
+package localization;
+
+import java.util.Locale;
+
+class DefaultLocale {
+    public static void main(String[] args) {
+        Locale locale = Locale.getDefault();
+        System.out.println(locale);
+    }
+}
