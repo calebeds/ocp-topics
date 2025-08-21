548ccf4ffbb89c1dbe491fce51942bdee709049f
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-08-21 15:04:21 -0300

    add jdbc connection example

--- src/main/java/jdbc/BankService.java
+++ src/main/java/jdbc/BankService.java
@@ -1,4 +1,28 @@
 package jdbc;
 
-public class BankService {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+class BankService {
+    private static Connection connection;
+
+    public static void main(String[] args) {
+        connect();
+    }
+
+    public static void connect() {
+        try {
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/app", "root", "my-secret-pw");
+            System.out.println("Db Connection OK!");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+//        catch (ClassNotFoundException e) {
+//            throw new RuntimeException(e);
+//        }
+    }
+
 }
