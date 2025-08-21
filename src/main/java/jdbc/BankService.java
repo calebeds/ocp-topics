eb94999da485e11877c0c2ef3f22121d2b471264
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-08-21 17:11:59 -0300

    add retrieve one example

--- src/main/java/jdbc/BankService.java
+++ src/main/java/jdbc/BankService.java
@@ -2,6 +2,8 @@ package jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 class BankService {
@@ -9,6 +11,9 @@ class BankService {
 
     public static void main(String[] args) {
         connect();
+        retrieveOne();
+        System.out.println();
+        retrieveAll();
     }
 
     public static void connect() {
@@ -25,4 +30,39 @@ class BankService {
 //        }
     }
 
+    static void retrieveOne() {
+        System.out.println(getAccountDetails("123456", "12345678"));
+    }
+
+    static void retrieveAll() {
+
+    }
+
+    private static BankAccount getAccountDetails(final String branchCode, final String accountNo) {
+        final String selectSQL = "SELECT * FROM app.BANK_TABLE WHERE (BRANCH_CODE = ? AND ACCOUNT_NUMBER = ?)";
+        BankAccount bankAccount = null;
+
+        try(PreparedStatement preparedStatement = connection.prepareStatement(selectSQL)) {
+            preparedStatement.setString(1, branchCode);
+            preparedStatement.setString(2, accountNo);
+
+            ResultSet resultSet = preparedStatement.executeQuery(); // implies SELECT
+
+            if(!resultSet.next()) {
+                return bankAccount;
+            }
+
+            bankAccount = new BankAccount(
+                    resultSet.getString("BRANCH_CODE"),
+                    resultSet.getString(2),// "ACCOUNT_NUMBER"
+                    resultSet.getString("CUST_NAME"),
+                    resultSet.getString("CUST_ADDRESS"),
+                    resultSet.getDouble("BALANCE")
+            );
+        } catch (SQLException sqlException) {
+            throw new RuntimeException(sqlException);
+        }
+
+        return bankAccount;
+    }
 }
