2b7d93ff42a3a72521df9258993dc092d7366da7
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-09-12 08:53:04 -0300

    add question about inheritance

---
+++ src/main/java/questions/classdesign/Q2_1722.java
@@ -0,0 +1,25 @@
+package questions.classdesign;
+
+interface Classic {
+    int version = 1;
+    void read();
+}
+
+class MediaReader implements Classic {
+    int version = 2;
+
+    @Override
+    public void read() {
+        System.out.println(Classic.version);
+        System.out.println(((Classic) this).version);
+    }
+}
+
+
+
+public class Q2_1722 {
+    public static void main(String[] args) {
+        MediaReader mr = new MediaReader();
+        mr.read();
+    }
+}
