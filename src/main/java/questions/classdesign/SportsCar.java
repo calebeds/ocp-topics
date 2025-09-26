8fa22bc2fa128403a25e4afef9c8c030a9c16ff8
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-09-26 14:28:10 -0300

    add polimorphism example

---
+++ src/main/java/questions/classdesign/SportsCar.java
@@ -0,0 +1,20 @@
+package questions.classdesign;
+
+class Car {
+    public int gearRatio = 8;
+    public String accelerate() {
+        return "Accelerate : Car";
+    }
+}
+
+class SportsCar extends Car {
+    public int gearRatio = 9;
+    public String accelerate() {
+        return "Accelerate : SportsCar";
+    }
+
+    public static void main(String[] args) {
+        Car car = new SportsCar();
+        System.out.println(car.gearRatio + " " + car.accelerate());
+    }
+}
