2184d85cd69869f62286ed98cbe6ed4c4c6d075f
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-09-29 18:10:46 -0300

    add interfaces example

---
+++ src/main/java/questions/advancedclassdesign/Q2_1771.java
@@ -0,0 +1,28 @@
+package questions.advancedclassdesign;
+
+class TestClass implements T1, T2 {
+
+    @Override
+    public void m1() {
+
+    }
+}
+
+interface T1 {
+    int VALUE = 1;
+    void m1();
+}
+
+interface T2 {
+    int VALUE = 2;
+    void m1();
+}
+
+class Q2_1771 {
+    public static void main(String[] args) {
+        TestClass testClass = new TestClass();
+        System.out.println(((T1) testClass).VALUE);
+        System.out.println(((T2) testClass).VALUE);
+        System.out.println(testClass.m1(););
+    }
+}
