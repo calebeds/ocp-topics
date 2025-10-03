7e8404c3a5880731cdc7d5ee46b27fc362995f35
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-10-02 21:32:44 -0300

    add foreach question

---
+++ src/main/java/questions/lambdaandfi/Q2_1801.java
@@ -0,0 +1,17 @@
+package questions.lambdaandfi;
+
+import java.util.Arrays;
+import java.util.List;
+
+class Q2_1801 {
+    public static void main(String[] args) {
+        List<StringBuilder> messages = Arrays.asList(new StringBuilder(), new StringBuilder());
+
+        messages.stream().forEach(s -> s.append("helloworld"));
+
+        messages.forEach(s -> {
+            s.insert(5, ",");
+            System.out.println(s);
+        });
+    }
+}
