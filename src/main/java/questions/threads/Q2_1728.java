49239fade2319ef405e59fe11220a9137ee2cc7a
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-10-15 21:54:44 -0300

    add thread question

--- src/main/java/questions/threads/Q2_1728.java
+++ src/main/java/questions/threads/Q2_1728.java
@@ -1,4 +1,20 @@
 package questions.threads;
 
-public class Q2_1728 {
+class Q2_1728 {
+    static class Runner implements Runnable {
+
+        @Override
+        public void run() {
+            System.out.println("In Run");
+        }
+    }
+
+    public static void main(String[] args) {
+        Runner runner = new Runner();
+
+        Thread thread1 = new Thread(runner);
+        Thread thread2 = new Thread(runner);
+        thread1.start();
+        thread2.start();
+    }
 }
